89fb189d4e00f38cc41086d106a95c758a4e9947
Nishant singh <38534128+Nishantsingh11@users.noreply.github.com>
2022-11-11 19:52:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extends:true}))
 app.use('/posts',postRoutes)
 app.use('/user',userRoutes)
+const CONNECTION_URL = 'mongodb+srv://Nishanttravel:Nishanttravel@cluster0.v3sjrmv.mongodb.net/?retryWrites=true&w=majorityt';
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.mongodb://Nishanttravel:Nishanttravel@cluster0.v3sjrmv.mongodb.net/?retryWrites=true&w=majority,{useNewUrlParser:true,useUnifiedTopology :true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology :true})
 .then(()=> app.listen(PORT,()=>console.log(`server is running on ${PORT}`)))
 .catch((error)=> console.log(error) )
 

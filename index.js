54100058cdb030ce1d31c47a935a64ebec5a8bea
Nishant singh <38534128+Nishantsingh11@users.noreply.github.com>
2022-11-11 20:01:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ app.use('/posts',postRoutes)
 app.use('/user',userRoutes)
 const CONNECTION_URL = 'mongodb+srv://Nishanttravel:Nishanttravel@cluster0.v3sjrmv.mongodb.net/?retryWrites=true&w=majorityt';
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology :true})
+
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology :true})
 .then(()=> app.listen(PORT,()=>console.log(`server is running on ${PORT}`)))
 .catch((error)=> console.log(error) )
+
+
+
 
